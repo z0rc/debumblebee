fe7afdd15e611930f7b444b47132bdcc95bf386b
Igor Urazov <z0rc3r@gmail.com>
2011-05-30 18:21:01 +0300

    Fixed typo in acpi_call install script. Closes #2.

--- install-files/contrib/acpi_call/install.sh
+++ install-files/contrib/acpi_call/install.sh
@@ -4,4 +4,4 @@ mkdir /usr/src/acpi_call-0.0.1
 cp ./* /usr/src/acpi_call-0.0.1
 dkms add -m acpi_call -v 0.0.1
 dkms build -m acpi_call -v 0.0.1
-dksm install -m acpi_call -v 0.0.1
+dkms install -m acpi_call -v 0.0.1
