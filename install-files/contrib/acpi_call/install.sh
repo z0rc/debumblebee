a96b567859d46f49c93093b89b1530d69a821d03
Igor Urazov <z0rc3r@gmail.com>
2011-08-04 22:57:03 +0300

    Add acpi_call to tree as submodule

--- install-files/contrib/acpi_call/install.sh
+++ install-files/contrib/acpi_call/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir /usr/src/acpi_call-0.0.1
-cp ./* /usr/src/acpi_call-0.0.1
+cp dkms.conf src/* /usr/src/acpi_call-0.0.1
 dkms add -m acpi_call -v 0.0.1
 dkms build -m acpi_call -v 0.0.1
 dkms install -m acpi_call -v 0.0.1
